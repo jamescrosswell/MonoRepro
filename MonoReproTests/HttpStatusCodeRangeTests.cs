af35031826e8d42302bf20366d9ffddeb26e5329
James Crosswell <jamescrosswell@users.noreply.github.com>
2025-06-11 20:52:42 +1200

    Initial commit

---
+++ MonoReproTests/HttpStatusCodeRangeTests.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * This code has been adapted from the HttpStatusCodeRangeTests from the Sentry SDK for .NET.
+ * https://github.com/getsentry/sentry-dotnet/blob/4162cad5e2d9c0aa39ac7e13f7f3644f4ea92925/test/Sentry.Tests/HttpStatusCodeRangeTests.cs#L51-L64
+ */
+using FluentAssertions;
+using MonoRepro;
+
+namespace MonoReproTests;
+
+public class HttpStatusCodeRangeTests
+{
+    [Fact]
+    public void HttpStatusCodeRange_Excludes_AboveEnd()
+    {
+        // Arrange
+        var start = 100;
+        var end = 200;
+        HttpStatusCodeRange sut = (start, end);
+
+        // Act
+        var inRange = sut.Contains(201);
+
+        // Assert
+        inRange.Should().BeFalse();
+    }
+}
